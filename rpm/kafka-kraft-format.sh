892bc2d61024ca51dd11b31e2d31b4b8c637d8c4
doraeven <doraeven@163.com>
2024-07-07 23:16:57 +0800

    update env config

--- rpm/kafka-kraft-format.sh
+++ rpm/kafka-kraft-format.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# tmp env PATH
-source /etc/sysconfig/kafka-kraft
+# env
+export CLASSPATH=.:/usr/lib64/kafka/*
+export KAFKA_SERVER_CONFIG=/etc/kafka/kraft/server.properties
+export KAFKA_LOG4J_OPTS=-Dlog4j.configuration=file:///etc/kafka/tools-log4j.properties
 
 # Generate a Cluster UUID
 KAFKA_CLUSTER_ID="$(/usr/bin/kafka/kafka-storage.sh random-uuid)"
@@ -11,3 +13,4 @@ echo "KAFKA_CLUSTER_ID is:" $KAFKA_CLUSTER_ID
 
 # Format Log Directories
 /usr/bin/kafka/kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c $KAFKA_SERVER_CONFIG
+
