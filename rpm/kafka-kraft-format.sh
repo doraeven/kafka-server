394c9c248bc83886fb89007a2824309eb6d5a97f
doraeven <doraeven@163.com>
2024-07-08 16:56:01 +0800

    update kafka-run-class.sh patch

--- rpm/kafka-kraft-format.sh
+++ rpm/kafka-kraft-format.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # env
-export CLASSPATH=.:/usr/lib64/kafka/*
 export KAFKA_SERVER_CONFIG=/etc/kafka/kraft/server.properties
-export KAFKA_LOG4J_OPTS=-Dlog4j.configuration=file:///etc/kafka/tools-log4j.properties
+# After Patch, not need to add
+# export CLASSPATH=.:/usr/lib64/kafka/*
+# export KAFKA_LOG4J_OPTS=-Dlog4j.configuration=file:///etc/kafka/tools-log4j.properties
 
 # Generate a Cluster UUID
 KAFKA_CLUSTER_ID="$(/usr/bin/kafka/kafka-storage.sh random-uuid)"
@@ -13,4 +14,3 @@ echo "KAFKA_CLUSTER_ID is:" $KAFKA_CLUSTER_ID
 
 # Format Log Directories
 /usr/bin/kafka/kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c $KAFKA_SERVER_CONFIG
-
